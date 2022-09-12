4736bbae8c8c0d7e4b88e787d88201d3a0b683ac
Masoud Zeinalabedini <masoud.zeinalabedini@gmail.com>
2022-09-12 15:21:24 +0330

    Complete Contact component

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,10 +1,81 @@
 import React from 'react';
 import './contact.css';
+import {MdOutlineMail} from 'react-icons/md'
+import {RiMessengerLine} from 'react-icons/ri'
+import {BsWhatsapp} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Contact = () => {
+
+    const notify = () => toast.success("Email sent!",{
+        theme: "dark"
+    });
+
+    const form:any = useRef();
+
+    const sendEmail = (e:any) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('masoud-contact', 'template_9mn2wir', form.current, 'CrjYs_4Qr36nkqtUW')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+
+        e.target.reset();
+        notify();
+      };
+    
+
     return (
         <section id='contact'>
-            contact
+            <h5>Get In Touch</h5>
+            <h2>Contact Me</h2>
+
+            <div className="container contact__container">
+                <div className="contact__options">
+                <article className="contact__option">
+                    <MdOutlineMail className='contact__option-icon' />
+                    <h4>Email</h4>
+                    <h5>Masoud.Zeinalabedini@gmail.com</h5>
+                    <a href="mailto:masoud.zeinalabedini@gmail.com" target="_blank" rel="noreferrer" >
+                        Send a message
+                    </a>
+                </article>
+
+                <article className="contact__option">
+                    <RiMessengerLine className='contact__option-icon' />
+                    <h4>Messenger</h4>
+                    <h5>MAsoud</h5>
+                    <a href="https://m.me/masoud" target="_blank" rel="noreferrer" >
+                        Send a message
+                    </a>
+                </article>
+
+                <article className="contact__option">
+                    <BsWhatsapp className='contact__option-icon' />
+                    <h4>WhatsApp</h4>
+                    <h5>+98 914 845 0108</h5>
+                    <a href="https://wa.me/+989148450108" target="_blank" rel="noreferrer" >
+                        Send a message
+                    </a>
+                </article>
+                </div>
+            
+
+                <form ref={form} onSubmit={sendEmail}>
+                    <input type="text" name="name" placeholder="Your Name" required/>
+                    <input type="email" name="email" placeholder="Your Email" required/>
+                    <textarea name="message" rows={7} placeholder="Your Message" required></textarea>
+                    <button type="submit" className='btn btn-primary'>Send Message</button>
+                    <ToastContainer autoClose={1000} position="top-center"/>
+                </form>
+
+            </div>
         </section>
     );
 };
