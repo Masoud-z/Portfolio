454cd9543a2f3ffcca0edf1d9693b36ceb24129c
Masoud Zeinalabedini <masoud.zeinalabedini@gmail.com>
2022-09-12 16:02:46 +0330

    Complete Footer component

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
 import './footer.css';
+import {BsLinkedin, BsInstagram} from'react-icons/bs';
+import {BsGithub} from 'react-icons/bs';
+import {BsTwitter} from 'react-icons/bs';
 
 const Footer = () => {
     return (
-        <section id='footer'>
-            footer
-        </section>
+        <footer>
+            <a href="#" className='footer__logo'>MASOUD</a>
+
+            <ul className="permalinks">
+                <li><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#experience">Experience</a></li>
+                <li><a href="#services">Services</a></li>
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#testimonials">Testimonials</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+
+            <div className="footer__socials">
+                <a href="https://github.com"> <BsGithub/> </a>
+                <a href="https://linkedin.com"> <BsLinkedin/> </a>
+                <a href="https://twitter.com"> <BsTwitter/> </a>
+                <a href="https://instagram.com"> <BsInstagram/> </a>
+            </div>
+        </footer>
     );
 };
 
