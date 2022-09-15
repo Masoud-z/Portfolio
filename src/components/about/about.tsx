02a823c598250cd9c00c48e2db5754baeded8268
pooya Games <426713+pooyaEst@users.noreply.github.com>
2022-09-15 16:42:58 +0430

    Scroll detects reaching About Me section

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,60 +1,80 @@
-import React, { useRef } from 'react';
-import './about.css';
-import me from '../../assets/me-about.jpg';
-import {FaAward} from 'react-icons/fa';
-import {FiUsers} from 'react-icons/fi'
-import {VscFolderLibrary} from 'react-icons/vsc'
+import React, { useEffect, useRef } from "react";
+import "./about.css";
+import me from "../../assets/me-about.jpg";
+import { FaAward } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
-    
-    const test:any = useRef();
-    
-    
+	const test: any = useRef();
+	const sectionRef = useRef<HTMLTableSectionElement>(null);
+	const hasAboutReached = useRef<boolean>(false);
+	useEffect(() => {
+		window.addEventListener("scroll", scrollBehavior);
+		return () => {
+			window.removeEventListener("scroll", scrollBehavior);
+		};
+	}, []);
 
-    return (
-        <section id='about'>
-            <h5 ref={test}>Get To Know</h5>
-            <h2>About Me</h2>
+	const scrollBehavior = () => {
+		if (
+			!hasAboutReached.current &&
+			window.scrollY + window.innerHeight >
+				(sectionRef.current?.offsetTop as number)
+		) {
+			hasAboutReached.current = true;
+			(document.querySelector("section h2") as HTMLHeadingElement).innerText =
+				"About Me dynamically changed";
+		}
+	};
 
-            <div className="container about__container">
-                <div className="about__me">
-                    <div className="about__me-image">
-                        <img src={me} alt="Masoud" />
-                    </div>
-                </div>
+	return (
+		<section id="about" ref={sectionRef}>
+			<h5 ref={test}>Get To Know</h5>
+			<h2>About Me</h2>
 
-                <div className="about__content">
-                    <div className="about__cards">
-                        <article className="about__card">
-                            <FaAward className='about__icon'/>
-                           <h5>Experience</h5>
-                           <small>3+ Years Working</small> 
-                        </article>
-               
-                        <article className="about__card">
-                            <FiUsers className='about__icon'/>
-                           <h5>Clients</h5>
-                           <small>200+ WorldWide</small> 
-                        </article>
-                  
-                        <article className="about__card">
-                            <VscFolderLibrary className='about__icon'/>
-                           <h5>Projects</h5>
-                           <small>80+ Completed </small> 
-                        </article>
-                    </div>
+			<div className="container about__container">
+				<div className="about__me">
+					<div className="about__me-image">
+						<img src={me} alt="Masoud" />
+					</div>
+				</div>
 
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro, debitis similique, saepe doloremque sapiente necessitatibus ut reiciendis alias beatae totam atque ad minus excepturi quod quaerat hic earum numquam aspernatur?</p>
+				<div className="about__content">
+					<div className="about__cards">
+						<article className="about__card">
+							<FaAward className="about__icon" />
+							<h5>Experience</h5>
+							<small>3+ Years Working</small>
+						</article>
 
-                    <a
-                     href="#contact"
-                     className='btn btn-primary'>
-                        Let's Talk
-                    </a>
-                </div>
-            </div>
-        </section>
-    );
+						<article className="about__card">
+							<FiUsers className="about__icon" />
+							<h5>Clients</h5>
+							<small>200+ WorldWide</small>
+						</article>
+
+						<article className="about__card">
+							<VscFolderLibrary className="about__icon" />
+							<h5>Projects</h5>
+							<small>80+ Completed </small>
+						</article>
+					</div>
+
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro,
+						debitis similique, saepe doloremque sapiente necessitatibus ut
+						reiciendis alias beatae totam atque ad minus excepturi quod quaerat
+						hic earum numquam aspernatur?
+					</p>
+
+					<a href="#contact" className="btn btn-primary">
+						Let's Talk
+					</a>
+				</div>
+			</div>
+		</section>
+	);
 };
 
 export default About;
