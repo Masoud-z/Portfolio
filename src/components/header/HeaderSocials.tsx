f1835bd727e50fe18542f8e225a7bf4fff8e59f3
Masoud Zeinalabedini <masoud.zeinalabedini@gmail.com>
2022-09-18 14:17:02 +0330

    Correct the size of icons in HeaderSocials component

--- src/components/header/HeaderSocials.tsx
+++ src/components/header/HeaderSocials.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import {BsLinkedin} from'react-icons/bs';
 import {BsGithub} from 'react-icons/bs';
-import {BsTwitter} from 'react-icons/bs';
-import {MdOutlineMail} from 'react-icons/md'
+import {AiFillTwitterCircle} from 'react-icons/ai';
+import {HiMail} from 'react-icons/hi'
 
 const HeaderSocials = () => {
     return (
         <div className='header__socials'>
-            <a href="https://www.linkedin.com/in/masoud-zeinalabedini/" target="_blank" className='linked'>
+            <a href="https://www.linkedin.com/in/masoud-zeinalabedini/" target="_blank" className='linked' rel="noreferrer">
                 <BsLinkedin />
             </a>
 
-            <a href="https://github.com/Masoud-z" target="_blank" className='git'>
+            <a href="https://github.com/Masoud-z" target="_blank" className='git' rel="noreferrer">
                 <BsGithub />
             </a>
             
-            <a href="https://twitter.com/Masoud_Zin" target="_blank" className='twit'>
-                <BsTwitter />
+            <a href="https://twitter.com/Masoud_Zin" target="_blank" className='twit' rel="noreferrer">
+                <AiFillTwitterCircle />
             </a>
 
-            <a href="mailto:masoud.zeinalabedini@gmail.com" target="_blank" rel="noreferrer" >
-                <MdOutlineMail />
+            <a href="mailto:masoud.zeinalabedini@gmail.com" rel="noreferrer" className='mail'>
+                <HiMail />
             </a>
         </div>
     );
