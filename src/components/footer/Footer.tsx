d76478ca4361954e814964f040c0cddd5c406ff7
Masoud Zeinalabedini <masoud.zeinalabedini@gmail.com>
2022-09-18 12:32:12 +0330

    Eddit Home link in Footer component

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -10,10 +10,10 @@ const Footer = () => {
             <a href="#" className='footer__logo'>MASOUD</a>
 
             <ul className="permalinks">
-                <li><a href="#">Home</a></li>
+                <li><a href="#header">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#experience">Experience</a></li>
-                <li><a href="#services">Services</a></li>
+                {/* <li><a href="#services">Services</a></li> */}
                 <li><a href="#portfolio">Portfolio</a></li>
                 <li><a href="#testimonials">Testimonials</a></li>
                 <li><a href="#contact">Contact</a></li>
