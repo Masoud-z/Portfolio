d0bbe2098f978c8d00850d02939b32ce802aefa0
Masoud Zeinalabedini <masoud.zeinalabedini@gmail.com>
2022-09-20 21:59:50 +0330

    Give animation to text border in About component

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,11 +1,13 @@
 import './about.css';
 import me from '../../assets/me-about.jpg';
-import {FaAward} from 'react-icons/fa';
-import {FiUsers} from 'react-icons/fi'
-import {VscFolderLibrary} from 'react-icons/vsc'
 import CV from '../../assets/cv.pdf';
+import { useContext, useState } from 'react';
+import { scrolledComponent } from '../../helper/scrolled';
 
 const About = () => {
+
+    const {scrolled, setScrolled}: any = useContext(scrolledComponent);
+
     
     return (
         <section id='about'>
@@ -16,7 +18,7 @@ const About = () => {
                 
                 <div className="about__content">
                     
-                    <div  className='border'>
+                    <div  className={scrolled==="#about"?'border':''}>
                         <p>I'm always looking for opportunities to expand upon my skills and am currently seeking a remote full-time junior web developer position or internship.</p>
                     </div>
 
