66c1f310e82fa1f15a46b852b5709d9cb9230bca
Masoud <masoud.zeinalabedini@gmail.com>
2023-03-31 12:07:05 +0330

    Change whatsapp link to Telegram link in Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,50 +1,66 @@
-import React from 'react';
-import './footer.css';
-import {BsLinkedin, BsWhatsapp} from'react-icons/bs';
-import {BsGithub} from 'react-icons/bs';
-import {BsTwitter} from 'react-icons/bs';
-import {MdOutlineMail} from 'react-icons/md'
+import React from "react";
+import "./footer.css";
+import { BsLinkedin, BsTelegram } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
+import { BsTwitter } from "react-icons/bs";
+import { MdOutlineMail } from "react-icons/md";
 
 const Footer = () => {
-    return (
-        <footer>
-            
-            <a className='footer__logo'>MASOUD</a>
+  return (
+    <footer>
+      <a className="footer__logo">MASOUD</a>
 
-            <ul className="permalinks">
-                <li><a href="#header">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#experience">Experience</a></li>
-                {/* <li><a href="#services">Services</a></li> */}
-                <li><a href="#portfolio">Portfolio</a></li>
-                {/* <li><a href="#testimonials">Testimonials</a></li> */}
-                <li><a href="#contact">Contact</a></li>
-            </ul>
+      <ul className="permalinks">
+        <li>
+          <a href="#header">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        {/* <li><a href="#services">Services</a></li> */}
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        {/* <li><a href="#testimonials">Testimonials</a></li> */}
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
 
-            <div className="footer__socials">
-                <a href="mailto:masoud.zeinalabedini@gmail.com" >
-                    <MdOutlineMail />
-                </a>
+      <div className="footer__socials">
+        <a href="mailto:masoud.zeinalabedini@gmail.com">
+          <MdOutlineMail />
+        </a>
 
-                <a href="https://www.linkedin.com/in/masoud-zeinalabedini/" target="_blank" rel="noreferrer">
-                     <BsLinkedin/> 
-                </a>
+        <a
+          href="https://www.linkedin.com/in/masoud-zeinalabedini/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsLinkedin />
+        </a>
 
-                <a href="https://wa.me/+989148450108" target="_blank" rel="noreferrer">
-                    <BsWhatsapp  />
-                </a>
+        <a href="https://t.me/M_Zinalabedini" target="_blank" rel="noreferrer">
+          <BsTelegram />
+        </a>
 
-                <a href="https://github.com/Masoud-z" target="_blank" rel="noreferrer">
-                     <BsGithub/> 
-                </a> 
+        <a href="https://github.com/Masoud-z" target="_blank" rel="noreferrer">
+          <BsGithub />
+        </a>
 
-                <a href="https://twitter.com/Masoud_Zin" target="_blank" rel="noreferrer">
-                     <BsTwitter/> 
-                </a>
-
-            </div>
-        </footer>
-    );
+        <a
+          href="https://twitter.com/Masoud_Zin"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <BsTwitter />
+        </a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
